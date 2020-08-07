158b167e5fccf8e0084e2ead3c99fe5f8d841826
IgorOluic <igoroluic13@gmail.com>
2020-08-07 17:17:11 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  theme: {
+    inset: {
+      '1/2': '50%',
+      '5px': '5px',
+      'm5px': '-5px',
+      '1/3': '33%',
+      '40px': '40px',
+      '0': '0',
+    },
+    height: {
+      '2px': '2px',
+      '10': '2.5rem',
+      '50px': '50px',
+    },
+    fontFamily: {
+      'monsterrat': 'Monsterrat'
+    },
+    borderRadius: {
+      '25px': '25px',
+      'lg': '0.5rem',
+    },
+  },
+  plugins: [
+
+  ],
+  variants: {
+    transitionProperty: ['responsive', 'motion-safe', 'motion-reduce']
+  }
+};
