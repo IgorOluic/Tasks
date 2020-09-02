7c56ee6311acccbfec180cd76a15e55e6116c8e8
IgorOluic <igoroluic13@gmail.com>
2020-09-02 17:57:23 +0200

    removed .then and .catch

--- controller/Login.js
+++ controller/Login.js
@@ -1,22 +1,14 @@
 import LoadingSpinner from "../view/LoadingSpinner.js";
 import loginValidator from "./LoginValidator.js";
-import request from "../model/Request.js";
-import Result from "../view/Result.js";
+import loginRequest from "../model/LoginRequest.js";
 
 function login() {
-  let result = new Result();
   let spinner = new LoadingSpinner();
   let data = loginValidator();
 
   if (data) {
     spinner.showSpinner();
-    request("https://dentministrator.herokuapp.com/auth/signin", data)
-      .then(() => {
-        result.loggedIn();
-      })
-      .catch((error) => {
-        result.notLoggedIn(error);
-      });
+    loginRequest("https://dentministrator.herokuapp.com/auth/signin", data);
   }
 }
 
